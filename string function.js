2d96f19419e30bcf26ac0c2bf06120b3840b3944
James Unekwuojo <121576934+Jamesunekwuojo@users.noreply.github.com>
2023-08-12 21:46:17 +0100

    background color changer

---
+++ string function.js
@@ -0,0 +1,23 @@
+/* charAt() 
+
+concat() 
+
+indexOf() 
+
+lastIndexOf() 
+
+split() 
+
+toUpperCase() 
+
+toLowerCase()  */
+var greet = "hello";
+console.log(greet.length);
+console.log(greet.charAt(1));
+console.log(greet.concat("ing").concat("is "));
+console.log(greet.indexOf("h"));
+console.log("ho-ho-ho".indexOf("-"));
+console.log("ho-ho-ho".lastIndexOf("o"));
+console.log(greet.split("h"));
+console.log("ho-ho-ho".split("-"));
+console.log("ho-ho-ho".toUpperCase());
