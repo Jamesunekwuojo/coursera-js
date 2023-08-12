08dd2ecf379c3546b72d2359eddaa4eb252b223a
James Unekwuojo <121576934+Jamesunekwuojo@users.noreply.github.com>
2023-08-12 02:54:14 +0100

    functions-js

---
+++ math.js
@@ -0,0 +1,29 @@
+// for pi - math.PI
+//Euler's constant -math.E
+// math.LN2 - natural logarithm of 2
+var oddNum = 12
+var estimate =oddNum* Math.PI;
+Math.ceil(estimate);
+console.log(Math.ceil(estimate));
+//Arithmetic and calculus methods
+/*
+Math.pow(2,3) - calculates the number 2 to the power of 3, the result is 8 
+
+Math.sqrt(16) - calculates the square root of 16, the result is 4 
+
+Math.cbrt(8) - finds the cube root of 8, the result is 2 
+
+Math.abs(-10) - returns the absolute value, the result is 10 
+
+Logarithmic methods: Math.log(), Math.log2(), Math.log10() 
+
+Return the minimum and maximum values of all the inputs: Math.min(9,8,7) returns 7, Math.max(9,8,7) returns 9.
+
+ Trigonometric methods: 
+ Math.sin(), Math.cos(),Math.tan(), etc.
+Math.random () - generates integers numbers between 0 and 0.99
+  */
+ var random = Math.random()*10;
+ console.log(random)
+
+
